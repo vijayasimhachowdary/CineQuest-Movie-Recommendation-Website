f0bd7fe0756576719dd1df54018890733ce843b9
vijayasimhachowdary <126192333+vijayasimhachowdary@users.noreply.github.com>
2025-04-04 21:43:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CineQuest-Movie-Recommendation-Website/', // 👈 Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
